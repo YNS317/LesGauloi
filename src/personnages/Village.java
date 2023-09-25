187548fd11d6cd671733cde8478a5d46e981b05a
YNS317 <145299495+YNS317@users.noreply.github.com>
2023-09-25 19:38:07 +0200

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,6 +3,8 @@ package personnages;
 public class Village {
     private String nom;
     private Chef chef;
+    private Gaulois[] villageois ;
+    private int nbVillageois = 0 ;
     public Village(String nom) {
          this.nom = nom;
     }
